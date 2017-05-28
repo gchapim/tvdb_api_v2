ce2212040f400a7693ec47186850ad56d32caf17
gchapim <gustavo@chapim.com>
2017-05-28 22:27:38 +0000

    Implementing logic for refreshing token for API

--- lib/tvdb_api/tvdb_api_wrapper.rb
+++ lib/tvdb_api/tvdb_api_wrapper.rb
@@ -8,12 +8,13 @@ class TvdbApiWrapper
     raise ArgumentError, 'api_key must be filled' if api_key.blank?
 
     response = token_from_login(api_key)
-    raise UnauthorizedError, 'Unauthorized' if response.code == 401
-    raise ConnectionError, "Couldn't connect to api" if ['200', '201']
-      .exclude?(response.code.to_s) || response.body.blank?
+    token_from_response(response)
+  end
 
-    json_hash = JSON.parse(response.body)
-    json_hash["token"]
+  def refresh_token(token)
+    raise ArgumentError, 'token must be filled' if token.blank?
+    response = get_response("/refresh_token", token)
+    token_from_response(response)
   end
 
   private
@@ -24,4 +25,21 @@ class TvdbApiWrapper
                                headers: { "Content-Type": "application/json",
                                          "Accept": "application/json"}})
   end
+
+  def get_response(action, token, params=nil)
+    response = HTTParty.get("#{TVDB_API_ADDRESS}#{action}",
+                             { headers: { "Content-Type": "application/json",
+                                         "Accept": "application/json",
+                                         "Authorization": "Bearer #{token}"}})
+  end
+
+  def token_from_response(response)
+    raise UnauthorizedError, 'Unauthorized' if response.code == 401
+    raise ConnectionError, "Couldn't connect to api" if ['200', '201']
+      .exclude?(response.code.to_s) || response.body.blank?
+
+    json_hash = JSON.parse(response.body)
+    json_hash["token"]
+  end
+
 end
--- spec/tvdb_api/tvdb_api_wrapper_spec.rb
+++ spec/tvdb_api/tvdb_api_wrapper_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe TvdbApiWrapper do
     context 'given a valid key' do
       it 'returns valid token' do
         VCR.use_cassette('valid_token') do
-          expect(subject.get_token('valid_token')).to be_present
+          expect(subject.get_token('valid_key')).to be_present
         end
       end
     end
@@ -42,7 +42,33 @@ RSpec.describe TvdbApiWrapper do
 
       it 'raises ConnectionError' do
         stub_request(:post, 'https://api.thetvdb.com/login').to_return({ status: 500 })
+
         expect {subject.get_token('valid_key')}.to raise_error(TheTvdbApi::ConnectionError)
+      end
+    end
+  end
+
+  describe '#refresh_token' do
+    context 'given an empty token' do
+      it 'raises ArgumentError' do
+        expect {subject.refresh_token(' ')}.to raise_error(ArgumentError)
+        expect {subject.refresh_token(nil)}.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'given an invalid token' do
+      it 'raises UnauthorizedError' do
+        stub_request(:post, 'https://api.thetvdb.com/refresh_token').to_return({ status: 401 })
+
+        expect {subject.refresh_token('invalid_key')}.to raise_error(TheTvdbApi::UnauthorizedError)
+      end
+    end
+
+    context 'given a valid token' do
+      it 'generates a new token' do
+        VCR.use_cassette('refresh_token') do
+          expect(subject.refresh_token("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0OTYwOTA5NTQsImlkIjoiTmV0c2VyaWVzIiwib3JpZ19pYXQiOjE0OTYwMDQ1NTR9.GtNGgdpMAHNaTvDxLU9JjReAVkWNdP9dhn85BPAeu0COAgaiuAkoT5RL3b8HaCz92C8oD7_t3w-MRyzX5pJEo3JwrOTQNIAqHjzgfzhusjPEci2kWzmyFjXlwSKb2OFXuuArmeooJuM76-ip9Fgrlnh773aL4a_Uniuy6PCFAqSF8jw_-_XYNhQZ2LZnVMedErObeowIUQqILtPGLF1Uxs5tYDrTAzNg97EQM7wrKMgU3VEAsEhLmntieqGlO-Z3iIm4tYIWh_IeG4bCK0BszkT1l37wn3ZBn5beD8bbsUL9yorGPAPyXw0G4O2KcqxtpDBsCIxZYOt8EyDz9I4BYQ")).to be_present
+        end
       end
     end
   end
