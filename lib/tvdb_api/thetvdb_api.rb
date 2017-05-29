d1a1757b3a86590bf6eaca2d799536aa5cf55fa4
Gustavo Chapim <gustavo.chapim@gmail.com>
2017-05-29 00:49:36 +0000

    Updating test

--- lib/tvdb_api/thetvdb_api.rb
+++ lib/tvdb_api/thetvdb_api.rb
@@ -2,6 +2,8 @@ module TheTvdbApi
 
   TVDB_API_ADDRESS = 'https://api.thetvdb.com'
 
+  ACTIONS = {login: "/login", refresh_token: "/refresh_token",
+             search: "/series/search"}
   class UnauthorizedError < StandardError; end
   class ConnectionError < StandardError; end
 end
