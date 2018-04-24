a967e6d7d322008033c6cabe7b2b1b89598b2bbe
Gustavo Chapim <gustavo@chapim.com>
2018-04-24 18:46:29 -0300

    Update thetvdb_api_v2.rb

--- lib/thetvdb_api_v2.rb
+++ lib/thetvdb_api_v2.rb
@@ -1,6 +1,6 @@
 require 'active_support/all'
-require 'tvdb_api/episode'
-require 'tvdb_api/show'
 require 'tvdb_api/thetvdb_api'
 require 'tvdb_api/tvdb_api_wrapper'
 require 'tvdb_api/tvdb_api'
+require 'tvdb_api/tvdb_episode'
+require 'tvdb_api/tvdb_show'
